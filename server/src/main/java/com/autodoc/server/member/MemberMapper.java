1146362efb85f5fbbd58c3ce694fc5449acedde8
Snu97 <snu970814@naver.com>
2023-08-06 13:02:19 +0900

    feat: member

---
+++ server/src/main/java/com/autodoc/server/member/MemberMapper.java
@@ -0,0 +1,9 @@
+package com.autodoc.server.member;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface MemberMapper {
+    Member memberPostToMember(MemberDto.Post post);
+}
