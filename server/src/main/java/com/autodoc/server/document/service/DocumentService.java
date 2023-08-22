dfb384989d276492ca3c375b1b63d60ee2103877
naraspc <wodnrtkfka@naver.com>
2023-08-22 23:25:06 +0900

    test

--- server/src/main/java/com/autodoc/server/document/service/DocumentService.java
+++ server/src/main/java/com/autodoc/server/document/service/DocumentService.java
@@ -28,7 +28,6 @@ public class DocumentService {
     }
 
     // create document
-
     public DocumentEntity create(DocumentDto dto) {
         DocumentEntity entity = dto.toEntity();
 
@@ -36,7 +35,6 @@ public class DocumentService {
             return null;
         }
 
-        //리턴
         return documentRepository.save(entity);
     }
 
